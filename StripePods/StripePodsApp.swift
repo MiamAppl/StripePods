b5bb55a4300a7f5ef703d0ba702d90607c506a36
BilelHattay <BilelHattay>
2021-06-23 15:43:32 +0200

    çabuild

---
+++ StripePods/StripePodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripePodsApp.swift
+//  StripePods
+//
+//  Created by Bilel Hattay on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct StripePodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
